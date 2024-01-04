e9ee51b3956a7e58abb05336ac0c9915df25bfd3
Nostalgicname <hawgsticker1234@gmail.com>
2024-01-04 11:02:24 -0600

    To do at home

---
+++ Code Projects/puzzle.java
@@ -0,0 +1,7 @@
+public class puzzle{
+    public static void main(String[] args)
+    {
+        // Original Sequence : 1, 2, 3 ,6, 12, 15, 21, 24, 30, 33, 39, 51, 57, 69, 84
+        // each sequence is 11 digits long, 
+    }
+}
