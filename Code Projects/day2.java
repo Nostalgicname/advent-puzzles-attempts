67d8214a3543f01012c051a48a520e648315d53f
Nostalgicname <hawgsticker1234@gmail.com>
2024-03-18 09:05:25 -0500

    Puzzle changes

--- Code Projects/day2.java
+++ Code Projects/day2.java
@@ -1,13 +1,16 @@
-import java.util.*;
-import java.io.*;
 
 public class day2 {
-    public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(new File("day2.txt"));
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        
+        int sum = arr[0];
+        int i = 0;
+        while (i < arr.length - 1)
+        {
+            i++;
+            System.out.println(i);
+            sum += arr[i];
+        }
 
-        while(in.hasNextLine()) {
-            String line = in.nextLine();
-            System.out.println(line);
         }
     }
-}
